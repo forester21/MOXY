25fda2d10e4dbff862bf9bfa3c62b136169d1d27
Egor Lesnyak <e.lesnyak@aladdin.store>
2026-01-03 14:41:02 +0300

    Refresh image by button click: hearts variants

---
+++ images/helper.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from PIL import Image
+
+prefix = "heart3"
+img = Image.open("./hearts/hearts3.png").convert("RGBA")
+pixels = img.load()
+
+rows = defaultdict(list)
+
+for y in range(img.height):
+    for x in range(img.width):
+        if pixels[x, y][3] > 0:
+            rows[y].append(x)
+
+print(f"short {prefix}Y[] = {{")
+i = 0
+for y, xs in rows.items():
+    print(f"        {y}, {len(xs)},")
+    i = i + 1
+print("};")
+print(f"short {prefix}X[] = {{")
+for y, xs in rows.items():
+    print(f"        {xs}".replace("[", "").replace("]", "") + ",")
+print("};")
+
+print(f"short {prefix}Size = {i};")
